72a44295d80e53c5cd626dcd040cf97a3b825c6d
MichaelArestad <marestad@gmail.com>
2014-10-19 16:30:31 -0600

    Added pretend portfolio and very basic styles

---
+++ portfolio-image.php
@@ -0,0 +1,9 @@
+<article class="portfolio-item">
+	<header class="entry-header">
+		<h1 class="entry-title screen-reader-text"><a href="http://themes.dev/?p=188" rel="bookmark">Layout Test</a></h1>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<a href="#" class="media-icon photo"><span class="screen-reader-text">View</span></a>
+	</div><!-- .entry-content -->
+</article>
---
+++ portfolio.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template Name: Portfolio
+ *
+ * @package Open
+ */
+
+get_header(); ?>
+
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main portfolio" role="main">
+
+		<?php include 'portfolio-image.php' ?>
+		<?php include 'portfolio-image.php' ?>
+		<?php include 'portfolio-image.php' ?>
+		<?php include 'portfolio-image.php' ?>
+		<?php include 'portfolio-image.php' ?>
+		<?php include 'portfolio-image.php' ?>
+		<?php include 'portfolio-image.php' ?>
+		<?php include 'portfolio-image.php' ?>
+		<?php include 'portfolio-image.php' ?>
+		<?php include 'portfolio-image.php' ?>
+		<?php include 'portfolio-image.php' ?>
+		<?php include 'portfolio-image.php' ?>
+		<?php include 'portfolio-image.php' ?>
+		<?php include 'portfolio-image.php' ?>
+		<?php include 'portfolio-image.php' ?>
+		<?php include 'portfolio-image.php' ?>
+		<?php include 'portfolio-image.php' ?>
+		<?php include 'portfolio-image.php' ?>
+		<?php include 'portfolio-image.php' ?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
